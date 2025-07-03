a5df3a9cc554b08a44a104334b160531d7d12659
fatimafais <fatimafaisal223@gmail.com>
2025-07-03 12:43:52 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,64 @@
+<footer class="footer">
+  <div class="footer-container">
+    <hr class="footer-top-line" /> 
+    <div class="footer-columns">
+      <div class="footer-col">
+        <h4>Models</h4>
+        <ul>
+          <li><a href="models/MGPHEV.php">MG HS PHEV</a></li>
+          <li><a href="models/MG5.php">MG 5</a></li>
+          <li><a href="models/MGCYBERSTER.php">MG CYBERSTER</a></li>
+          <li><a href="models/MG4.php">MG 4</a></li>
+          <li><a href="models/MGZSEV.php">MG ZS EV</a></li>
+        </ul>
+      </div>
+      <div class="footer-col">
+        <h4>Other Links</h4>
+        <ul>
+          <li>Dealer Locator</li>
+          <li>MG Care</li>
+          <li>Book a Test Drive</li>
+          <li>Book My MG</li>
+          <li>World of MG</li>
+          <li>Track My MG</li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h4>Legal</h4>
+        <ul>
+          <li>Cookie Policy</li>
+          <li>End User License Agreement</li>
+          <li>Consent Settings</li>
+          <li>Privacy Policy</li>
+        </ul>
+      </div>
+
+      <div class="footer-col">
+        <h4>Contact</h4>
+        <ul>
+          <li>About MG</li>
+          <li>Contact Us</li>
+          <li>Careers</li>
+          <li>Become a Dealer</li>
+        </ul>
+      </div>
+    </div>
+  
+
+   <div class="footer-bottom">
+      <img src="images/mg-logo.png" class="mg-logo" alt="MG Logo" />
+      <div class="social-icons">
+        <img src="images/facebook.png" alt="Facebook" />
+        <img src="images/instagram.png" alt="Instagram" />
+        <img src="images/linkedin.png" alt="LinkedIn" />
+        <img src="images/youtube.png" alt="YouTube" />
+      </div>
+    </div>
+  </div>
+</footer>
+  <a href="https://wa.me/1234567890" class="whatsapp-float" target="_blank">
+    <img src="images/whatsapp.png" alt="WhatsApp" />
+  </a>
+
+
